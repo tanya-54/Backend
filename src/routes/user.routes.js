7d1fa9725b459b2fc28ed586641900cce357dbea
Tanya Dubey <dubeytanya2312@gmail.com>
2024-10-11 15:27:45 +0530

    added secured routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
  import { Router } from "express";
- import { registerUser } from "../controllers/user.controller.js";
+ import { loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
  import { upload } from "../middlewares/multer.middleware.js";
  import express from 'express';
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
- const router = Router()
+const router = Router()
 
  router.route("/register").post(
-    upload.fields([   //uploads is coming from multer so it provides various methods  & now we can send images .
+    upload.fields([   //uploads is coming from multer so it provides various methods  & now we can send images.
         {
             name:"avatar",
             maxCount: 1
@@ -17,5 +18,11 @@
         }
      ]),
  registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured Routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refreshToken").post(refreshAccessToken)
 
  export default router;
